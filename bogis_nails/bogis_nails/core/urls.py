e039b6e7fd80ad608fb9259fca5486d4e6049fa8
Yordan Yordanov (S. T. G.) <y.yordanov.stg@gmail.com>
2024-03-27 20:35:16 +0200

    Update price list page.

--- bogis_nails/bogis_nails/core/urls.py
+++ bogis_nails/bogis_nails/core/urls.py
@@ -1,20 +1,13 @@
 from django.urls import path
 from django.conf.urls import handler404
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-
 from bogis_nails.core.views import \
     index, custom_404, price_list
 
-urlpatterns = [
+urlpatterns = (
     path('', index, name='index'),
     
     path('price_list/', price_list, name='price list'),
-]
+)
 
 handler404 = custom_404 
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
